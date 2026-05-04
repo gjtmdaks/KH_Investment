640a46c268b2d2d28d075d317192347cc9dbfe53
LAPTOP-LF0G491J\sim81 <sim8191si@naver.com>
2026-05-04 14:40:23 +0900

    기초토대

---
+++ investSpring/src/main/java/com/kh/investSpring/InvestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kh.investSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestSpringApplication.class, args);
+	}
+
+}
