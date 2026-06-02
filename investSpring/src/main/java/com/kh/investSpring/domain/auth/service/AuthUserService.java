b4a394e8806e97cf69bd09e18b8d9fe5d938be00
DESKTOP-FJBRFI8\user1 <inho156@naver.com>
2026-06-02 11:21:23 +0900

    카카오 계정 내계좌 만들기

--- investSpring/src/main/java/com/kh/investSpring/domain/auth/service/AuthUserService.java
+++ investSpring/src/main/java/com/kh/investSpring/domain/auth/service/AuthUserService.java
@@ -1,54 +1,33 @@
 package com.kh.investSpring.domain.auth.service;
 
-
-
 import org.springframework.stereotype.Service;
-
 import org.springframework.transaction.annotation.Transactional;
 
-
-
+import com.kh.investSpring.domain.account.dao.AccountDao;
 import com.kh.investSpring.domain.auth.dao.AuthDao;
-
-
 
 import lombok.RequiredArgsConstructor;
 
-
-
 @Service
-
 @RequiredArgsConstructor
-
 public class AuthUserService {
 
-
-
 	private final AuthDao authDao;
-
-
+	private final AccountDao accountDao;
 
 	@Transactional
-
 	public Long resolveOrCreateKakaoUser(String providerId, String displayName) {
-
 		Long userNo = authDao.findUserNoByKakaoProviderId(providerId);
-
 		if (userNo != null) {
-
 			return userNo;
-
 		}
 
 		long newUserNo = authDao.nextUserNo();
-
 		authDao.insertKakaoUser(newUserNo, displayName);
-
 		authDao.insertUserSocial(providerId, newUserNo);
+		accountDao.insertAccount((int) newUserNo);
+		accountDao.insertAccountBalance((int) newUserNo);
 
 		return newUserNo;
-
 	}
-
 }
-
